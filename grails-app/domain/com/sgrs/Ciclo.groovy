795de50c2409004277c28017877d0c4f92c4ded7
balmore <balmoreortiz17@gmail.com>
2021-11-11 17:58:24 -0600

    modificaciones a Modelo: Ciclo y Reserva

--- grails-app/domain/com/sgrs/Ciclo.groovy
+++ grails-app/domain/com/sgrs/Ciclo.groovy
@@ -2,8 +2,8 @@ package com.sgrs
 
 class Ciclo {
 
-    Integer id_ciclo
-    String name
+    Integer id_ciclo        //e.g id_ciclo: 1
+    String name             //e.g name: CICLO IV
 
     static hasMany = [materia: Materia]
 
--- grails-app/domain/com/sgrs/Reserva.groovy
+++ grails-app/domain/com/sgrs/Reserva.groovy
@@ -5,7 +5,13 @@ class Reserva {
     String name         //e.g name: "primer examen parcial Fisico-Química I"
     String fecha        //e.g fecha: "13-nov-2021"
 
-    static belongsTo = [locales: Local, materias: Materia, evaluaciones: Evaluacion, horarios: Horario, estados: Estado]
+    static belongsTo = [
+        /*todo esto es accedido por medio de sus campos identificadores + toString()*/
+        locales: Local,             //eg. local: B22
+        materias: Materia,          //eg. materia: TOO115
+        evaluaciones: Evaluacion,   //e.g evaluación: Examen Parcial
+        horarios: Horario,          //e.g horario: "6:20-8:15"
+        estados: Estado]            //e.g estado: pendiente
 
     static constraints = {
         name size: 5..100, blank:false
